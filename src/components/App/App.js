265a3d0563daab502c73a35d3642824d5cb10cf4
solarenergy990 <74872776+solarenergy990@users.noreply.github.com>
2021-08-27 14:02:34 +0200

    creates base components

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,12 @@
-function App() {
-  return <div className="App"></div>;
-}
+import React from 'react';
+import HomePage from '../HomePage/HomePage';
+
+const App = () => {
+  return (
+    <div>
+      <HomePage />
+    </div>
+  );
+};
 
 export default App;
---
+++ src/components/HomePage/HomePage.js
@@ -0,0 +1,173 @@
+import React from 'react';
+import s from '../HomePage/HomePage.module.css';
+
+const HomePage = () => {
+  return (
+    <div>
+      <section className={s.section}>
+        <div className={s.galleryContainer}>
+          <ul className={s.gallery}>
+            <li className={s.galleryItemCard}>
+              <div className={s.posterContainer}>
+                <img
+                  //   className="poster"
+                  src="https://fs.kinomania.ru/file/film_poster/4/e4/4e41d549faa224ee50aa7307c48adf19.jpeg"
+                  alt="Poster-film"
+                />
+              </div>
+              <div>
+                <h2 className={s.textTitle}>Harry Potter</h2>
+                <p className={s.textGenre}>Drama, Other | 2019</p>
+              </div>
+            </li>
+            <li className={s.galleryItemCard}>
+              <div className={s.posterContainer}>
+                <img
+                  className="poster"
+                  src="https://fs.kinomania.ru/file/film_poster/4/e4/4e41d549faa224ee50aa7307c48adf19.jpeg"
+                  alt="Poster-film"
+                />
+              </div>
+              <div>
+                <h2 className={s.textTitle}>Harry Potter</h2>
+                <p className={s.textGenre}>Drama, Other | 2019</p>
+              </div>
+            </li>
+            <li className={s.galleryItemCard}>
+              <div className={s.posterContainer}>
+                <img
+                  className="poster"
+                  src="https://fs.kinomania.ru/file/film_poster/4/e4/4e41d549faa224ee50aa7307c48adf19.jpeg"
+                  alt="Poster-film"
+                />
+              </div>
+              <div>
+                <h2 className={s.textTitle}>Harry Potter</h2>
+                <p className={s.textGenre}>Drama, Other | 2019</p>
+              </div>
+            </li>
+            <li className={s.galleryItemCard}>
+              <div className={s.posterContainer}>
+                <img
+                  className="poster"
+                  src="https://fs.kinomania.ru/file/film_poster/4/e4/4e41d549faa224ee50aa7307c48adf19.jpeg"
+                  alt="Poster-film"
+                />
+              </div>
+              <div>
+                <h2 className={s.textTitle}>Harry Potter</h2>
+                <p className={s.textGenre}>Drama, Other | 2019</p>
+              </div>
+            </li>
+            <li className={s.galleryItemCard}>
+              <div className={s.posterContainer}>
+                <img
+                  className="poster"
+                  src="https://fs.kinomania.ru/file/film_poster/4/e4/4e41d549faa224ee50aa7307c48adf19.jpeg"
+                  alt="Poster-film"
+                />
+              </div>
+              <div>
+                <h2 className={s.textTitle}>Harry Potter</h2>
+                <p className={s.textGenre}>Drama, Other | 2019</p>
+              </div>
+            </li>
+            <li className={s.galleryItemCard}>
+              <div className={s.posterContainer}>
+                <img
+                  className="poster"
+                  src="https://fs.kinomania.ru/file/film_poster/4/e4/4e41d549faa224ee50aa7307c48adf19.jpeg"
+                  alt="Poster-film"
+                />
+              </div>
+              <div>
+                <h2 className={s.textTitle}>Harry Potter</h2>
+                <p className={s.textGenre}>Drama, Other | 2019</p>
+              </div>
+            </li>
+            <li className={s.galleryItemCard}>
+              <div className={s.posterContainer}>
+                <img
+                  className="poster"
+                  src="https://fs.kinomania.ru/file/film_poster/4/e4/4e41d549faa224ee50aa7307c48adf19.jpeg"
+                  alt="Poster-film"
+                />
+              </div>
+              <div>
+                <h2 className={s.textTitle}>Harry Potter</h2>
+                <p className={s.textGenre}>Drama, Other | 2019</p>
+              </div>
+            </li>
+            <li className={s.galleryItemCard}>
+              <div className={s.posterContainer}>
+                <img
+                  className="poster"
+                  src="https://fs.kinomania.ru/file/film_poster/4/e4/4e41d549faa224ee50aa7307c48adf19.jpeg"
+                  alt="Poster-film"
+                />
+              </div>
+              <div>
+                <h2 className={s.textTitle}>Harry Potter</h2>
+                <p className={s.textGenre}>Drama, Other | 2019</p>
+              </div>
+            </li>
+            <li className={s.galleryItemCard}>
+              <div className={s.posterContainer}>
+                <img
+                  className="poster"
+                  src="https://fs.kinomania.ru/file/film_poster/4/e4/4e41d549faa224ee50aa7307c48adf19.jpeg"
+                  alt="Poster-film"
+                />
+              </div>
+              <div>
+                <h2 className={s.textTitle}>Harry Potter</h2>
+                <p className={s.textGenre}>Drama, Other | 2019</p>
+              </div>
+            </li>
+            <li className={s.galleryItemCard}>
+              <div className={s.posterContainer}>
+                <img
+                  className="poster"
+                  src="https://fs.kinomania.ru/file/film_poster/4/e4/4e41d549faa224ee50aa7307c48adf19.jpeg"
+                  alt="Poster-film"
+                />
+              </div>
+              <div>
+                <h2 className={s.textTitle}>Harry Potter</h2>
+                <p className={s.textGenre}>Drama, Other | 2019</p>
+              </div>
+            </li>
+            <li className={s.galleryItemCard}>
+              <div className={s.posterContainer}>
+                <img
+                  className="poster"
+                  src="https://fs.kinomania.ru/file/film_poster/4/e4/4e41d549faa224ee50aa7307c48adf19.jpeg"
+                  alt="Poster-film"
+                />
+              </div>
+              <div>
+                <h2 className={s.textTitle}>Harry Potter</h2>
+                <p className={s.textGenre}>Drama, Other | 2019</p>
+              </div>
+            </li>
+            <li className={s.galleryItemCard}>
+              <div className={s.posterContainer}>
+                <img
+                  className="poster"
+                  src="https://fs.kinomania.ru/file/film_poster/4/e4/4e41d549faa224ee50aa7307c48adf19.jpeg"
+                  alt="Poster-film"
+                />
+              </div>
+              <div>
+                <h2 className={s.textTitle}>Harry Potter</h2>
+                <p className={s.textGenre}>Drama, Other | 2019</p>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default HomePage;
